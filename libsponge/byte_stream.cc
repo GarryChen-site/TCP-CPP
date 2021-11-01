e2d2debba98d9344a191c2da81dda955bc44ab63
GarryChen <chenymj23@gmail.com>
2021-11-01 20:45:43 +0800

    lab0 done

--- libsponge/byte_stream.cc
+++ libsponge/byte_stream.cc
@@ -1,5 +1,8 @@
 #include "byte_stream.hh"
 
+#include <algorithm>
+#include <iterator>
+
 // Dummy implementation of a flow-controlled in-memory byte stream.
 
 // For Lab 0, please replace with a real implementation that passes the
@@ -12,42 +15,109 @@ void DUMMY_CODE(Targs &&... /* unused */) {}
 
 using namespace std;
 
-ByteStream::ByteStream(const size_t capacity) { DUMMY_CODE(capacity); }
+ByteStream::ByteStream(const size_t capacity) :_capacity(capacity) {}
 
 size_t ByteStream::write(const string &data) {
-    DUMMY_CODE(data);
-    return {};
+    size_t len = data.length();
+    if(len > _capacity - _buffer.size())
+    {
+        len = _capacity - _buffer.size();
+    }
+    _write_count += len;
+    for(size_t i=0; i<len; i++)
+    {
+        _buffer.push_back(data[i]);
+    }
+    return len;
+    
 }
 
 //! \param[in] len bytes will be copied from the output side of the buffer
 string ByteStream::peek_output(const size_t len) const {
-    DUMMY_CODE(len);
-    return {};
+    size_t length = len;
+    if(length > _buffer.size())
+    {
+        length = _buffer.size();
+    }
+    
+    return string().assign(_buffer.begin(),_buffer.begin()+length);
 }
 
 //! \param[in] len bytes will be removed from the output side of the buffer
-void ByteStream::pop_output(const size_t len) { DUMMY_CODE(len); }
+void ByteStream::pop_output(const size_t len) 
+{ 
+    size_t length = len;
+    if(length > _buffer.size())
+    {
+        length = _buffer.size();
+    } 
+    _read_count += length;
+    while(length--)
+    {
+        _buffer.pop_front();
+    }
+    return;
+}
 
 //! Read (i.e., copy and then pop) the next "len" bytes of the stream
 //! \param[in] len bytes will be popped and returned
 //! \returns a string
 std::string ByteStream::read(const size_t len) {
-    DUMMY_CODE(len);
-    return {};
+    // size_t length = len;
+    // if(length > _buffer.size())
+    // {
+    //     length = _buffer.size();
+    // } 
+    // _read_count += length;
+    // while(length--) // length is negitive , assign will throw an exception
+    // {
+    //     _buffer.pop_front();
+    // }
+    // return string().assign(_buffer.begin(),_buffer.begin()+length);
+
+    std::string data = peek_output(len);
+
+    pop_output(len);
+
+    return data;
 }
 
-void ByteStream::end_input() {}
+void ByteStream::end_input() 
+{
+    _input_ended_flag = true;
+}
 
-bool ByteStream::input_ended() const { return {}; }
+bool ByteStream::input_ended() const 
+{ 
+    return _input_ended_flag; 
+}
 
-size_t ByteStream::buffer_size() const { return {}; }
+size_t ByteStream::buffer_size() const 
+{
+     return _buffer.size(); 
+}
 
-bool ByteStream::buffer_empty() const { return {}; }
+bool ByteStream::buffer_empty() const 
+{
+     return _buffer.size() == 0; 
+}
 
-bool ByteStream::eof() const { return false; }
+bool ByteStream::eof() const 
+{ 
+    return buffer_empty() && input_ended(); 
+}
 
-size_t ByteStream::bytes_written() const { return {}; }
+size_t ByteStream::bytes_written() const 
+{
+     return _write_count; 
+}
 
-size_t ByteStream::bytes_read() const { return {}; }
+size_t ByteStream::bytes_read() const 
+{ 
+    return _read_count; 
+}
 
-size_t ByteStream::remaining_capacity() const { return {}; }
+size_t ByteStream::remaining_capacity() const 
+{ 
+    return _capacity - _buffer.size(); 
+}
--- libsponge/byte_stream.hh
+++ libsponge/byte_stream.hh
@@ -2,6 +2,7 @@
 #define SPONGE_LIBSPONGE_BYTE_STREAM_HH
 
 #include <string>
+#include <deque>
 
 //! \brief An in-order byte stream.
 
@@ -10,14 +11,14 @@
 //! and then no more bytes can be written.
 class ByteStream {
   private:
-    // Your code here -- add private members as necessary.
 
-    // Hint: This doesn't need to be a sophisticated data structure at
-    // all, but if any of your tests are taking longer than a second,
-    // that's a sign that you probably want to keep exploring
-    // different approaches.
+    std::deque<char> _buffer = {};
+    size_t _capacity = 0;
+    size_t _read_count = 0;
+    size_t _write_count = 0;
+    bool _input_ended_flag = false;
 
-    bool _error{};  //!< Flag indicating that the stream suffered an error.
+    bool _error = false;  //!< Flag indicating that the stream suffered an error.
 
   public:
     //! Construct a stream with room for `capacity` bytes.
