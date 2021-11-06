4209b8e862f20aad30cb1bc1939ef54ee9d7d8c2
garry <chenymj23@gmail.com>
2021-11-06 23:29:09 +0800

    webget done

--- apps/webget.cc
+++ apps/webget.cc
@@ -11,7 +11,7 @@ void get_URL(const string &host, const string &path) {
 
     TCPSocket sock;
     sock.connect(Address(host,"http"));
-    sock.write("GET " +path+ "HTTP/1.1\r\nHost: " + host + "\r\n\r\n");
+    sock.write("GET " +path+ " HTTP/1.1\r\nHost: " + host + "\r\n\r\n");
     sock.shutdown(SHUT_WR);
     while (!sock.eof())
     {
