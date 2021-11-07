325201a1cfdb2fc962af9aac55697c04ddc7fd51
garry <chenymj23@gmail.com>
2021-11-07 17:18:54 +0800

    wrap + unwrap done

--- libsponge/wrapping_integers.cc
+++ libsponge/wrapping_integers.cc
@@ -1,5 +1,6 @@
 #include "wrapping_integers.hh"
 
+
 // Dummy implementation of a 32-bit wrapping integer
 
 // For Lab 2, please replace with a real implementation that passes the
@@ -14,8 +15,8 @@ using namespace std;
 //! \param n The input absolute 64-bit sequence number
 //! \param isn The initial sequence number
 WrappingInt32 wrap(uint64_t n, WrappingInt32 isn) {
-    DUMMY_CODE(n, isn);
-    return WrappingInt32{0};
+
+    return isn + (static_cast<uint32_t>(n)) ;
 }
 
 //! Transform a WrappingInt32 into an "absolute" 64-bit sequence number (zero-indexed)
@@ -29,6 +30,17 @@ WrappingInt32 wrap(uint64_t n, WrappingInt32 isn) {
 //! and the other stream runs from the remote TCPSender to the local TCPReceiver and
 //! has a different ISN.
 uint64_t unwrap(WrappingInt32 n, WrappingInt32 isn, uint64_t checkpoint) {
-    DUMMY_CODE(n, isn, checkpoint);
-    return {};
+    uint32_t offset = n - isn;
+    // checkpoint is the last sequence number that was received, so we need to move last 32 bits of the sequence number
+    // add the offset to the last 32 bits of the sequence number
+    uint64_t t = (checkpoint & 0xFFFFFFFF00000000) + offset;
+    uint64_t bigger = t + (1ul << 32) - checkpoint;
+    uint64_t smaller = t - (1ul << 32) - checkpoint;
+    uint64_t normal = t-checkpoint;
+    if(abs(int64_t(bigger)) < abs(int64_t(normal)))
+        t += 1ul<<32;
+    // make sure smaller not overflow
+    if(t >= (1ul<<32) && abs(int64_t(smaller)) < abs(int64_t(normal)))
+        t -= 1ul<<32;
+    return t;
 }
